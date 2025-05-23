7f4d34f30fc0135d4ceabcf8a5c3f212de83c121
Muhammad Imran Sarwar <mmanisarwar@gmail.com>
2025-05-23 16:31:06 +0500

    [Fix] Singleton

--- dev_mrp_ext/models/models.py
+++ dev_mrp_ext/models/models.py
@@ -67,22 +67,22 @@ class MrpProduction(models.Model):
     workcenter_id = fields.Many2one('mrp.workcenter', string="Work Center")
     operation_id = fields.Many2one('mrp.routing.workcenter', string="Operation")
 
-    # def write(self, values):
-    #     """
-    #     Override the write method to customize the update of 'batch_output' and 'batch_number'
-    #     when 'product_qty' is updated.
-    #     """
+    def write(self, values):
+        """
+        Override the write method to customize the update of 'batch_output' and 'batch_number'
+        when 'product_qty' is updated.
+        """
         # If product_qty is being updated, calculate batch_output and batch_number
-        # if 'product_qty' in values:
+        if 'product_qty' in values:
             # Update batch_output by dividing product_qty by 270
             # values['batch_output'] = values['product_qty'] / 270
-            # batch_output = values['product_qty'] / self.bom_id.batch_output
+            batch_output = values['product_qty'] / self.bom_id.batch_output
 
             # Round up the batch_output to the nearest whole number using ceil (round up)
-            # values['batch_output'] = ceil(batch_output)
+            values['batch_output'] = ceil(batch_output)
 
             # Update batch_number (you can modify this logic as needed)
-            # values['batch_number'] = values.get('product_qty', 0)  # Use product_qty or default to 0 if missing
+            values['batch_number'] = values.get('product_qty', 0)  # Use product_qty or default to 0 if missing
         # else:
         #     batch_output = self.product_qty / self.bom_id.batch_output
         #
@@ -93,7 +93,7 @@ class MrpProduction(models.Model):
 
 
         # Call the parent write method to ensure all other logic is executed
-        # return super(MrpProduction, self).write(values)
+        return super(MrpProduction, self).write(values)
 
     # def action_confirm(self):
     #     res = super().action_confirm()
@@ -101,20 +101,31 @@ class MrpProduction(models.Model):
     #     self.batch_output = ceil(batch_output)
     #     return res
 
-    # @api.model
-    # def create(self, values):
-    #     """
-    #     Override the create method to ensure 'batch_output' and 'batch_number' are set
-    #     based on 'product_qty' during record creation.
-    #     """
-    #     if 'product_qty' in values:
-    #         bom_id = values['bom_id']
-    #         bom = self.env['mrp.bom'].browse(bom_id)
+    def action_confirm(self):
+        res = super().action_confirm()
+        for record in self:
+            if record.bom_id and record.bom_id.batch_output not in (0, None):
+                batch_output = record.product_qty / record.bom_id.batch_output
+                record.batch_output = ceil(batch_output)
+            # else: silently skip or optionally log/debug
+        return res
+
+    @api.model
+    def create(self, values):
+        """
+        Override the create method to ensure 'batch_output' and 'batch_number' are set
+        based on 'product_qty' during record creation.
+        """
+        if 'product_qty' in values:
+            bom_id = values['bom_id']
+            bom = self.env['mrp.bom'].browse(bom_id)
 
             # Calculate batch_output by dividing product_qty by the bom's batch_output factor
-            # if bom.batch_output:
-            #    values['batch_output'] = bom.product_qty / bom.batch_output
-            #    return super(MrpProduction, self).create(values)
+            # values['batch_output'] = bom.product_qty / bom.batch_output
+            if bom and bom.batch_output not in (0, None):
+                # Safe division and rounding
+                batch_output = values['product_qty'] / bom.batch_output
+                values['batch_output'] = ceil(batch_output)
 
             # Round up the batch_output to the nearest whole number using ceil (round up)
             # values['batch_output'] = ceil(batch_output)
@@ -123,5 +134,6 @@ class MrpProduction(models.Model):
             # values['batch_number'] = values.get('product_qty', 0)
 
         # Call the parent create method to create the record
+        return super(MrpProduction, self).create(values)
 
 
