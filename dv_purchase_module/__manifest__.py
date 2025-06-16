ff5d0f585a2368f010d6c3fe6b4c7dec25481ffe
DigitalVizta <digitalvizta@gmail.com>
2025-06-16 14:42:19 +0500

    Purchase Product Vendor Filter

---
+++ dv_purchase_module/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Product Vendor Filter',
+    'version': '1.0',
+    'category': 'Purchases',
+    'author': 'DigitalVizta',
+    'website': 'https://digitalvizta.com',
+    'summary': 'Filter products in Purchase Order based on selected Vendor',
+    'description': """
+        This module filters the product list in purchase order lines.
+        - If no vendor is selected: show all products.
+        - If a vendor is selected: show only products where the vendor is listed in the product's purchase tab.
+    """,
+    'depends': ['purchase','stock'],
+    'data': [
+        'views/purchase_order.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
+
