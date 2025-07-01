21c5152bc7720e0b2f83242a6650cf30b4fd2aa7
Muhammad Imran Sarwar <mmanisarwar@gmail.com>
2025-07-01 22:14:31 +0500

    dv_purchase_module

--- dv_purchase_module/__manifest__.py
+++ dv_purchase_module/__manifest__.py
@@ -7,7 +7,7 @@
     'website': 'https://digitalvizta.com',
     'summary': 'Filter products in Purchase Order based on selected Vendor',
     'description': """
-        This module filters the product list in purchase order lines.
+        This module filters the product list in purchase order lines..
         - If no vendor is selected: show all products.
         - If a vendor is selected: show only products where the vendor is listed in the product's purchase tab.
     """,
