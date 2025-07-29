7051948a4cdf0ef6f76fbc4bf57ae3caf073c390
Muhammad Imran Sarwar <mmanisarwar@gmail.com>
2025-07-29 20:49:29 +0500

    Week Show

--- sale_order_extension/models/sale_order.py
+++ sale_order_extension/models/sale_order.py
@@ -11,6 +11,21 @@ class SaleOrder(models.Model):
         help='After completion, enter the date the order was actually picked up or delivered.',
         required=False
     )
+
+    actual_ship_week_number = fields.Char(
+        string='Week Number',
+        compute='_compute_actual_ship_week_number',
+        store=True
+    )
+
+    @api.depends('actual_ship_date')
+    def _compute_actual_ship_week_number(self):
+        for order in self:
+            if order.actual_ship_date:
+                order.actual_ship_week_number = f"Week {order.actual_ship_date.isocalendar()[1]}"
+            else:
+                order.actual_ship_week_number = ''
+
     pickup_week_number = fields.Char(string='Week Number', compute='_compute_week_number', store=True)
 
     @api.depends('pickup_delivery_datetime')
@@ -20,7 +35,23 @@ class SaleOrder(models.Model):
                 order.pickup_week_number = f"Week {order.pickup_delivery_datetime.isocalendar()[1]}"
             else:
                 order.pickup_week_number = ''
+
     fulfill_order_date = fields.Datetime(string='Date of Order')
+
+    fulfill_order_week_number = fields.Char(
+        string='Week Number',
+        compute='_compute_fulfill_order_week_number',
+        store=True
+    )
+
+    @api.depends('fulfill_order_date')
+    def _compute_fulfill_order_week_number(self):
+        for record in self:
+            if record.fulfill_order_date:
+                record.fulfill_order_week_number = f"Week {record.fulfill_order_date.isocalendar()[1]}"
+            else:
+                record.fulfill_order_week_number = ''
+
     po_no = fields.Char(string='PO No.')
     bol = fields.Char(string='BOL (Bill of Lading)',)
     lot_no = fields.Char(string='Lot No.', )
@@ -44,5 +75,33 @@ class SaleOrder(models.Model):
                     raise ValidationError("Pick Up / Delivery Date & Time must be after Date of Order.")
                 if order.actual_ship_date and order.actual_ship_date < order.fulfill_order_date:
                     raise ValidationError("Actual Ship Date must be after Date of Order.")
+
+    schedule_delivery_week_number = fields.Char(
+        string='Week Number',
+        compute='_compute_schedule_delivery_week_number',
+        store=True
+    )
+
+    @api.depends('schedule_delivery_date')
+    def _compute_schedule_delivery_week_number(self):
+        for record in self:
+            if record.schedule_delivery_date:
+                record.schedule_delivery_week_number = f"Week {record.schedule_delivery_date.isocalendar()[1]}"
+            else:
+                record.schedule_delivery_week_number = ''
+
+    date_order_week_number = fields.Char(
+        string='Week Number',
+        compute='_compute_date_order_week_number',
+        store=True
+    )
+
+    @api.depends('date_order')
+    def _compute_date_order_week_number(self):
+        for record in self:
+            if record.date_order:
+                record.date_order_week_number = f"Week {record.date_order.isocalendar()[1]}"
+            else:
+                record.date_order_week_number = ''
 
 
