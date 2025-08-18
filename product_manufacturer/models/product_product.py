1fbccdbcb3962deb2085b56fbccd71ab3972bef5
Muhammad Imran Sarwar <mmanisarwar@gmail.com>
2025-08-18 16:52:06 +0500

    SKU

--- product_manufacturer/models/product_product.py
+++ product_manufacturer/models/product_product.py
@@ -7,7 +7,12 @@ from odoo import fields, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    manufacturer_id = fields.Many2one(comodel_name="res.partner")
+    manufacturer_id = fields.Many2one(comodel_name="res.partner", string="Manufacturer")
     manufacturer_pname = fields.Char(string="Manufacturer Product Name")
-    manufacturer_pref = fields.Char(string="Manufacturer Product Code")
+    manufacturer_pref = fields.Char(string="Manufacturer BAR Code")
+    manufacturer_brand = fields.Char(string="Brand")
     manufacturer_purl = fields.Char(string="Manufacturer Product URL")
+    manufacturer_des = fields.Text(string="Description")
+    allgern_statement = fields.Char(string="Allgern Statement")
+    storage_tempature = fields.Char(string="Storage Tempature")
+    manufacturer_sku = fields.Char(string="Manufacturer SKU")
