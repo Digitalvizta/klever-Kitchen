6057888c694ee284a5f3d2b83f1073bdf98246c7
imran <muhammad.imran@evantage-soft.com>
2025-09-14 22:46:43 +0500

    Changes

--- dvz_generic_bom_substitute/__manifest__.py
+++ dvz_generic_bom_substitute/__manifest__.py
@@ -25,6 +25,7 @@ and maintains clean BoMs without hardcoding specific vendor products.
 
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/product_template_views.xml'
     ],
 
--- dvz_generic_bom_substitute/models/product_template.py
+++ dvz_generic_bom_substitute/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -11,3 +11,18 @@ class ProductTemplate(models.Model):
         string="Substitute Products",
         help="Actual inventory products that can be used instead of this generic product."
     )
+
+    product_category_type = fields.Selection([
+        ('mfg', 'MFG'),
+        ('fg', 'FG'),
+        ('vd', 'VD'),
+        ('rm', 'RM'),
+        ('sfg', 'SFG'),
+    ], string="Product Category Type", required=True)
+
+    auto_sequence_code = fields.Char(string="Auto Sequence Code", readonly=True, copy=False)
+
+    @api.onchange('product_category_type')
+    def _onchange_product_category_type(self):
+        if self.product_category_type:
+            self.auto_sequence_code = self.env['ir.sequence'].next_by_code(f'{self.product_category_type}_sequence')
